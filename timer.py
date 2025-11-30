81156a1ea5162d930510ffdabf83ddb401e0c203
jackfroot30 <jackfroot30@gmail.com>
2025-11-30 21:03:48 +0530

    updated timer

--- timer.py
+++ timer.py
@@ -1,7 +1,7 @@
-import sys
+```import sys
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QWidget
-from PyQt6.QtCore import QTimer
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt6.QtCore import QTimer, QPropertyAnimation
 # import everything !!
 
 
@@ -18,9 +18,39 @@ class UiTimer(QWidget):
         self.timer = QTimer(self)
         self.timer.setInterval(10)
         self.timer.timeout.connect(self._tick)
-
+        pushButton = self.pushButton
+        self.pushButton = HoverButton(pushButton.text(), pushButton.parent())
+        self.pushButton.setGeometry(pushButton.geometry())
+        self.pushButton.setStyleSheet(pushButton.styleSheet())      
+        self.pushButton.setFont(pushButton.font())                  
+        self.pushButton.setSizePolicy(pushButton.sizePolicy())      
+        self.pushButton.setCursor(pushButton.cursor())        
+        pushButton.hide()
+        self.pushButton.show()
         self.pushButton.clicked.connect(self.start)
+
+
+        pushButton_2 = self.pushButton_2
+        self.pushButton_2 = HoverButton(pushButton_2.text(), pushButton_2.parent())
+        self.pushButton_2.setGeometry(pushButton_2.geometry())
+        self.pushButton_2.setStyleSheet(pushButton_2.styleSheet())      
+        self.pushButton_2.setFont(pushButton_2.font())                  
+        self.pushButton_2.setSizePolicy(pushButton_2.sizePolicy())      
+        self.pushButton_2.setCursor(pushButton_2.cursor())        
+        pushButton_2.hide()
+        self.pushButton_2.show()
         self.pushButton_2.clicked.connect(self.pause)
+
+
+        pushButton_3 = self.pushButton_3
+        self.pushButton_3 = HoverButton(pushButton_3.text(), pushButton_3.parent())
+        self.pushButton_3.setGeometry(pushButton_3.geometry())
+        self.pushButton_3.setStyleSheet(pushButton_3.styleSheet())      
+        self.pushButton_3.setFont(pushButton_3.font())                  
+        self.pushButton_3.setSizePolicy(pushButton_3.sizePolicy())      
+        self.pushButton_3.setCursor(pushButton_3.cursor())        
+        pushButton_3.hide()
+        self.pushButton_3.show()
         self.pushButton_3.clicked.connect(self.reset)
 
         self.pushButton_2.setEnabled(False)
@@ -86,8 +116,34 @@ class UiTimer(QWidget):
 
         self._update_lcd()
 
+class HoverButton(QPushButton):
+    def __init__(self,*args):
+        super().__init__(*args)
+        self.base_geometry = None
+
+    def enterEvent(self, event):
+        if self.base_geometry is None:
+            self.base_geometry = self.geometry()
+
+        self.anim = QPropertyAnimation(self, b"geometry")
+        self.anim.setDuration(50)
+        self.anim.setStartValue(self.geometry())
+        self.anim.setEndValue(self.base_geometry.adjusted(-2, -2, 2, 2))  
+        self.anim.start()
+        super().enterEvent(event)
+
+    def leaveEvent(self, event):
+        self.anim = QPropertyAnimation(self, b"geometry")
+        self.anim.setDuration(150)
+        self.anim.setStartValue(self.geometry())
+        self.anim.setEndValue(self.base_geometry)
+        self.anim.start()
+        super().leaveEvent(event)
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     w = UiTimer("timer.ui")
     w.show()
     sys.exit(app.exec())
+```
